c1a8322469104c90c18afb0f6ae827adabf5f52c
sarpong <abasimisarpong@rocketmail.com>
2021-12-13 22:33:27 +0000

    Updated script to send many request -> Will be used with es

--- testServer.sh
+++ testServer.sh
@@ -2,12 +2,15 @@
 echo "Rule No1 - Ensure that the server is running 🎲"
 echo "Sending request to localhost:8888/api/v1/thoughts"
 
-curl --request POST -sL -i \
-     --header "Content-Type: application/json" \
-     --url 'http://localhost:8888/api/v1/thoughts'\
-     --data '{
-              "name" : "Nana Kumakumaa",
-              "message" : "I made a lot of progress with Scala today",
-              "regrets" : "I lived today to the fullest so I have no regrets"
-          }'
-          
+for i in {1..555}
+do
+  curl --request POST -sL -i \
+       --header "Content-Type: application/json" \
+       --url 'http://localhost:8888/api/v1/thoughts'\
+       --data '{
+                "name" : "Nana Kumakumaa",
+                "message" : "I made a lot of progress with Scala today",
+                "regrets" : "I lived today to the fullest so I have no regrets"
+            }'
+  echo "Request number $i "
+done
