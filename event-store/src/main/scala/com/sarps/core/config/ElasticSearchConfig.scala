7fe85958c62e53855a33c8a5743fe1b062faa196
sarpong <abasimisarpong@rocketmail.com>
2021-12-14 21:03:19 +0000

    Updated esConfig

--- event-store/src/main/scala/com/sarps/core/config/ElasticSearchConfig.scala
+++ event-store/src/main/scala/com/sarps/core/config/ElasticSearchConfig.scala
@@ -4,18 +4,43 @@ import ciris.{ConfigValue, Effect}
 import ciris.env
 import cats.implicits._
 
-final case class Index(index: String) extends AnyVal
-final case class Url(url: String)     extends AnyVal
+final case class Index(name: String)       extends AnyVal
+final case class Host(host: String)        extends AnyVal
+final case class Port(port: Int)           extends AnyVal
+final case class Scheme(scheme: String)    extends AnyVal
+final case class Shards(value: Int)        extends AnyVal
+final case class ShardReplicas(value: Int) extends AnyVal
+final case class Cluster(name: String)     extends AnyVal
 
 case class ElasticSearchConfig(
     index: Index,
-    url: Url
+    host: Host,
+    port: Port,
+    scheme: Scheme,
+    shards: Shards,
+    replicas: ShardReplicas,
+    cluster: Cluster
 )
 
 object ElasticSearchConfig {
   val elasticSearchConfig: ConfigValue[Effect, ElasticSearchConfig] =
     (
-      env("INDEX").as[String].default("thoughtsIndex"),
-      env("URL").as[String].default("")
-    ).mapN((index, url) => ElasticSearchConfig(Index(index), Url(url)))
+      env("INDEX").default("thoughtsIndex"),
+      env("HOST").default("localhost"),
+      env("PORT").as[Int].default(9200),
+      env("SCHEME").default("Http"),
+      env("NUMBER_OF_SHARDS").as[Int].default(3),
+      env("NUMBER_OF_REPLICAS").as[Int].default(2),
+      env("CLUSTER_NAME").as[String].default("elasticSearchCluster")
+    ).mapN((index, host, port, scheme, numberOfShards, replicas, cluster) =>
+      ElasticSearchConfig(
+        Index(index),
+        Host(host),
+        Port(port),
+        Scheme(scheme),
+        Shards(numberOfShards),
+        ShardReplicas(replicas),
+        Cluster(cluster)
+      )
+    )
 }
