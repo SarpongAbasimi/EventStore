ca69db2f465583f1c02cd6cf023d1bf12bca6245
sarpong <abasimisarpong@rocketmail.com>
2021-12-15 17:48:01 +0000

    Passed index service rather than consumer directly

--- event-store/src/main/scala/com/sarps/core/server/Server.scala
+++ event-store/src/main/scala/com/sarps/core/server/Server.scala
@@ -4,8 +4,10 @@ import cats.effect.ExitCode
 import cats.effect.kernel.Async
 import cats.effect.std.Console
 import com.sarps.core.config.Config
+import com.sarps.core.es.EsClientAlgebra
 import com.sarps.core.http.routes.ThoughtsRoute
 import com.sarps.core.kafka.{ThoughtsConsumer, ThoughtsProducer}
+import com.sarps.core.service.IndexingService
 import org.http4s.blaze.server.BlazeServerBuilder
 import fs2.Stream
 
@@ -13,13 +15,15 @@ object Server {
   def stream[F[_]: Async: Console](config: Config): fs2.Stream[F, ExitCode] = for {
     _ <- Stream.eval(Console[F].println("Starting Server 🚀"))
 
-    consumer: ThoughtsConsumer[F] = ThoughtsConsumer.impl[F](config.kafkaConfig)
-    producer: ThoughtsProducer[F] = ThoughtsProducer.impl[F](config.kafkaConfig)
+    esClient: EsClientAlgebra[F]     = EsClientAlgebra.impl[F](config.elasticSearchConfig)
+    producer: ThoughtsProducer[F]    = ThoughtsProducer.impl[F](config.kafkaConfig)
+    consumer: ThoughtsConsumer[F]    = ThoughtsConsumer.impl[F](config.kafkaConfig)
+    indexService: IndexingService[F] = IndexingService.impl[F](esClient, consumer)
 
     server <- BlazeServerBuilder[F]
       .bindHttp(config.serverConfig.port.port, config.serverConfig.host.host)
       .withHttpApp(ThoughtsRoute.appRoute[F](producer).orNotFound)
       .serve
-      .concurrently(consumer.consume)
+      .concurrently(indexService.persist)
   } yield server
 }
