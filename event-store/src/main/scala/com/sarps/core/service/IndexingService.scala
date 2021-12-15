578a9611db8d131272fe6072519f2c4b4c782f22
sarpong <abasimisarpong@rocketmail.com>
2021-12-15 17:49:08 +0000

    Updated indexService

--- event-store/src/main/scala/com/sarps/core/service/IndexingService.scala
+++ event-store/src/main/scala/com/sarps/core/service/IndexingService.scala
@@ -1,19 +1,36 @@
 package com.sarps.core.service
 
 import cats.effect.kernel.{Async, Resource}
+import cats.effect.std.Console
 import com.sarps.core.domain.Thoughts
 import com.sarps.core.es.EsClientAlgebra
+import com.sarps.core.kafka.ThoughtsConsumer
 import org.elasticsearch.client.RequestOptions
 import org.elasticsearch.client.indices.CreateIndexResponse
+import fs2.Stream
 
 trait IndexingService[F[_]] {
-  def persistEvent(event: Thoughts): Resource[F, CreateIndexResponse]
+  def persist: Stream[F, Unit]
 }
 
 object IndexingService {
-  def impl[F[_]: Async](esClientAlgebra: EsClientAlgebra[F]): IndexingService[F] =
+  def impl[F[_]: Async: Console](
+      esClientAlgebra: EsClientAlgebra[F],
+      consumer: ThoughtsConsumer[F]
+  ): IndexingService[F] =
     new IndexingService[F] {
-      def persistEvent(event: Thoughts): Resource[F, CreateIndexResponse] = for {
+
+      def persist: Stream[F, Unit] = {
+        consumer.consume.evalMapChunk(consumerRecord => {
+          persistEvent(consumerRecord.record.value).use { response =>
+            Console[F].println(
+              s"Message has been stored in E: isAcknowledged -> ${response.isAcknowledged}"
+            )
+          }
+        })
+      }
+
+      private[service] def persistEvent(event: Thoughts): Resource[F, CreateIndexResponse] = for {
         createIndex <- Resource.eval(esClientAlgebra.createIndexRequest)
         client      <- esClientAlgebra.client
         createIndexResponse <- Resource.eval(
