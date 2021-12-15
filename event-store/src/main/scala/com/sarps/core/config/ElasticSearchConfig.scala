84adb66585ec3da5919b7b9f4c3d9f2f9798ad1d
sarpong <abasimisarpong@rocketmail.com>
2021-12-15 17:45:11 +0000

    Removed clustername from config

--- event-store/src/main/scala/com/sarps/core/config/ElasticSearchConfig.scala
+++ event-store/src/main/scala/com/sarps/core/config/ElasticSearchConfig.scala
@@ -10,7 +10,6 @@ final case class Port(port: Int)           extends AnyVal
 final case class Scheme(scheme: String)    extends AnyVal
 final case class Shards(value: Int)        extends AnyVal
 final case class ShardReplicas(value: Int) extends AnyVal
-final case class Cluster(name: String)     extends AnyVal
 
 case class ElasticSearchConfig(
     index: Index,
@@ -18,29 +17,26 @@ case class ElasticSearchConfig(
     port: Port,
     scheme: Scheme,
     shards: Shards,
-    replicas: ShardReplicas,
-    cluster: Cluster
+    replicas: ShardReplicas
 )
 
 object ElasticSearchConfig {
   val elasticSearchConfig: ConfigValue[Effect, ElasticSearchConfig] =
     (
-      env("INDEX").default("thoughtsIndex"),
+      env("INDEX").default("thoughts-index"),
       env("HOST").default("localhost"),
       env("PORT").as[Int].default(9200),
       env("SCHEME").default("Http"),
       env("NUMBER_OF_SHARDS").as[Int].default(3),
-      env("NUMBER_OF_REPLICAS").as[Int].default(2),
-      env("CLUSTER_NAME").as[String].default("elasticSearchCluster")
-    ).mapN((index, host, port, scheme, numberOfShards, replicas, cluster) =>
+      env("NUMBER_OF_REPLICAS").as[Int].default(2)
+    ).mapN((index, host, port, scheme, numberOfShards, replicas) =>
       ElasticSearchConfig(
         Index(index),
         Host(host),
         Port(port),
         Scheme(scheme),
         Shards(numberOfShards),
-        ShardReplicas(replicas),
-        Cluster(cluster)
+        ShardReplicas(replicas)
       )
     )
 }
