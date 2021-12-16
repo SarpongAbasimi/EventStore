bfc843bfdf564044eb2ef127b04a6b3f2b27edfd
sarpong <abasimisarpong@rocketmail.com>
2021-12-16 16:04:26 +0000

    Added the restHighLevel client as a param ti the impl method

--- event-store/src/main/scala/com/sarps/core/service/IndexingService.scala
+++ event-store/src/main/scala/com/sarps/core/service/IndexingService.scala
@@ -1,26 +1,35 @@
 package com.sarps.core.service
 
-import cats.effect.kernel.{Async}
+import cats.effect.kernel.Async
 import cats.effect.std.Console
 import com.sarps.core.es.EsClientAlgebra
 import com.sarps.core.kafka.ThoughtsConsumer
+import org.elasticsearch.client.{RequestOptions, RestHighLevelClient}
 import fs2.Stream
+import cats.syntax.functor._
+import org.elasticsearch.action.index.IndexResponse
 
 trait IndexingService[F[_]] {
-  def persist: Stream[F, Unit]
+  def persist: Stream[F, IndexResponse]
 }
 
 object IndexingService {
   def impl[F[_]: Async: Console](
       esClient: EsClientAlgebra[F],
-      consumer: ThoughtsConsumer[F]
+      consumer: ThoughtsConsumer[F],
+      restClient: RestHighLevelClient
   ): IndexingService[F] =
     new IndexingService[F] {
 
-      def persist: Stream[F, Unit] = {
-        consumer.consume.evalMapChunk { _ =>
-          Async[F].delay()
-        }
+      def persist: Stream[F, IndexResponse] = {
+        consumer.consume
+          .evalMapChunk { consumedEvents =>
+            val indexRequest = esClient.index(consumedEvents.record.value)
+            indexRequest.map(request => restClient.index(request, RequestOptions.DEFAULT))
+          }
+          .evalTapChunk(indexResponse =>
+            Console[F].println(s"The index status is ->  ${indexResponse.status()}")
+          )
       }
     }
 }
