ce78713274b0918b116d9e5fbf232bd741502974
sarpong <abasimisarpong@rocketmail.com>
2021-12-17 17:02:48 +0000

    Made the necessary changes to reflect the esAlgebra

--- event-store/src/main/scala/com/sarps/core/server/Server.scala
+++ event-store/src/main/scala/com/sarps/core/server/Server.scala
@@ -4,7 +4,7 @@ import cats.effect.ExitCode
 import cats.effect.kernel.Async
 import cats.effect.std.Console
 import com.sarps.core.config.Config
-import com.sarps.core.es.EsClientAlgebra
+import com.sarps.core.es.{EsAlgebra, EsClientAlgebra}
 import com.sarps.core.http.routes.ThoughtsRoute
 import com.sarps.core.kafka.{ThoughtsConsumer, ThoughtsProducer}
 import com.sarps.core.service.IndexingService
@@ -16,13 +16,15 @@ object Server {
   def stream[F[_]: Async: Console](
       config: Config,
       restHighClient: RestHighLevelClient,
-      esClient: EsClientAlgebra[F]
+      esClient: EsClientAlgebra[F],
+      esAlgebra: EsAlgebra[F]
   ): fs2.Stream[F, ExitCode] = for {
     _ <- Stream.eval(Console[F].println("Starting Server 🚀"))
 
-    producer: ThoughtsProducer[F]    = ThoughtsProducer.impl[F](config.kafkaConfig)
-    consumer: ThoughtsConsumer[F]    = ThoughtsConsumer.impl[F](config.kafkaConfig)
-    indexService: IndexingService[F] = IndexingService.impl[F](esClient, consumer, restHighClient)
+    producer: ThoughtsProducer[F] = ThoughtsProducer.impl[F](config.kafkaConfig)
+    consumer: ThoughtsConsumer[F] = ThoughtsConsumer.impl[F](config.kafkaConfig)
+    indexService: IndexingService[F] = IndexingService
+      .impl[F](consumer, restHighClient, esAlgebra)
 
     server <- BlazeServerBuilder[F]
       .bindHttp(config.serverConfig.port.port, config.serverConfig.host.host)
